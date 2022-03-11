df67a2753716f4f254806141e5cba84ac4ad4114
Ohk Soe Htet <ohksoe2002@gmail.com>
2022-03-11 11:40:46 +0630

    code formatted and package.json modified

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import BasicForm from "./components/BasicForm";
 import SimpleInput from "./components/SimpleInput";
 
-function App() {
+const App = () => {
     return (
         <div className="app">
-            {/* <SimpleInput /> */}
-            <BasicForm />
+            <SimpleInput />
+            {/* <BasicForm /> */}
         </div>
     );
-}
+};
 
 export default App;
