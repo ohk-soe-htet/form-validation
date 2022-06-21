483454b59774316439f1fee7412097eaa5e4351a
Ohk Soe Htet <ohksoe2002@gmail.com>
2022-06-21 18:01:55 +0630

    added formik and yup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Form from "./components/basic/ReducerForm";
+import Basic from "./components/formik/BasicForm";
+// import SimpleInput from "./components/basic/SimpleInput";
+// import BasicForm from "./components/basic/BasicForm";
+
+const App = () => {
+    return (
+        <div className="app">
+            {/* <Form /> */}
+            <Basic />
+            {/* <SimpleInput /> */}
+            {/* <BasicForm /> */}
+        </div>
+    );
+};
+
+export default App;
